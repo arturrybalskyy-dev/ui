f6c90376f065f57194f5eb071b6380199e735ddc
Artur <artur.rybalskyy@globallogic.com>
2026-09-15 17:05:22 +0300

    [FIX] Sidebar not collapsing back after switching projects from dropdown

--- src/nextGenComponents/shared/Sidebar/ProjectDropdown/ProjectDropdown.jsx
+++ src/nextGenComponents/shared/Sidebar/ProjectDropdown/ProjectDropdown.jsx
@@ -53,7 +53,7 @@ import {
 const ProjectDropdown = ({ projectName }) => {
   const { pathname } = useLocation()
   const projectStore = useSelector(state => state.projectStore)
-  const { setHoverLocked } = useSidebar()
+  const { setHoverLocked, setOpen: setSidebarOpen } = useSidebar()
   const [open, setOpen] = useState(false)
   const [filter, setFilter] = useState('')
   const currentProjectRef = useRef(null)
@@ -101,6 +101,9 @@ const ProjectDropdown = ({ projectName }) => {
           onOpenChange={isOpen => {
             setOpen(isOpen)
             setHoverLocked(isOpen)
+            if (!isOpen) {
+              setSidebarOpen(false)
+            }
             setFilter('')
           }}
         >
